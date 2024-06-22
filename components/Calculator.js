56def9f97f8c6f8b2e5e0606cb46d3ecaea8f638
vishalkumar1007 <vishalkumarnke93@gmail.com>
2024-06-22 07:39:22 +0530

    60% work done , only write calculator logic

--- components/Calculator.js
+++ components/Calculator.js
@@ -1,169 +1,305 @@
 
-import { SafeAreaView, View, Text, Button, StyleSheet, TextInput } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { SafeAreaView, View, Text, StyleSheet, TextInput, Alert, TouchableOpacity, ScrollView } from 'react-native';
+import { Vibration } from 'react-native';
+import Svg, { Path, Line,Circle } from 'react-native-svg';
 
-const styles = StyleSheet.create({
-    calculator_main: {
-        // backgroundColor: 'black',
-        width: '94%',
-        height: '96%',
-        display:'flex',
-        rowGap: 20,
-    },
-    output_view: {
-        // backgroundColor: 'orange',
-        width: '100%',
-        height: '39%'
-    },
-    input_view: {
-        // backgroundColor: 'pink',
-        width: '100%',
-        height: '60%',
-        
-    },
-    main_btn_view:{
-        display:'flex',
-        flexDirection:'row',
-        flexWrap:'wrap',
-        width:'100%',
-        height:'100%',
-        justifyContent:'space-between',
-        rowGap:25,
-    },
-    cal_btn: {
-        width: 90,
-        height: 90,
-        backgroundColor: '#a5a5a5',
-        borderRadius: 50,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    cal_btn_text: {
-        fontSize: 40,
-        color: 'white'
-    },
-    cal_btn_operation: {
-        backgroundColor:'#ff9f0a',
-        color:'white',
-    },
-    cal_btn_number: {
-        backgroundColor:'#333333',
-        color:'white',
-    },
-    take_two_width:{
-        width:200,
-        height:100,
-    },
-    text_black:{
-        color:'#161616',
-    },
-    final_output:{
-        // backgroundColor:'red',
-        width:'100%',
-        height:'87%',
-        display:'flex',
-        justifyContent:'flex-end',
-        alignItems:'flex-end',
-        paddingRight:10,
-    },
-    final_output_text:{
-        // backgroundColor:'black',
-        fontSize:60,
-        color:'white',
-        
-    },
-    input_record:{
-        // backgroundColor:'green',
-        width:'100%',
-        height:'13%',
-        display:'flex',
-        justifyContent:'flex-end',
-        alignItems:'flex-end',
-        paddingRight:10,
-    },
-    input_record_text:{
-        fontSize:35,
-        color:'#eeeeee',
-    }
-})
 
 function Calculator() {
+    const scrollViewRef = useRef();
+    const [inputLog, setInputLog] = useState('');
+    const [finalOutput,setFinalOutput] = useState(0)
+
+    const HandelOnPress = (value) => {
+        Vibration.vibrate(10);
+        let input = inputLog;
+        input += value;
+        setInputLog(input);
+    }
+
+    const DeleteLog = (val) => {
+        Vibration.vibrate(10);
+        if(val==='AC'){
+            setInputLog('');
+        }else if(val==='X'){
+            setInputLog(inputLog.substr(0,inputLog.length-1))
+        }
+    }
+
+    useEffect(() => {
+        if (scrollViewRef.current) {
+            scrollViewRef.current.scrollToEnd({ animated: true });
+        }
+    }, [inputLog]);
+
+
     return (
         <SafeAreaView style={styles.calculator_main}>
             <View style={styles.output_view}>
-                <View style={styles.final_output}>
-                    <Text style={styles.final_output_text}>10+10</Text>
+                <View style={styles.final_output_container}>
+                    <ScrollView
+                        contentContainerStyle={styles.final_output}
+                        ref={scrollViewRef}
+                    >
+                        <Text style={styles.final_output_text}>{inputLog}</Text>
+                    </ScrollView>
                 </View>
                 <View style={styles.input_record}>
-                    <Text style={styles.input_record_text}>= 20</Text>
+                    <Text style={styles.input_record_text}>{finalOutput}</Text>
                 </View>
             </View>
             <View style={styles.input_view}>
                 <View style={styles.main_btn_view}>
-                    <View style={styles.cal_btn}>
-                        <Text style={[styles.cal_btn_text, styles.text_black]}>AC</Text>
-                    </View>
-                    <View style={styles.cal_btn}>
-                        <Text style={[styles.cal_btn_text, styles.text_black]}>+/-</Text>
-                    </View>
-                    <View style={styles.cal_btn}>
-                        <Text style={[styles.cal_btn_text, styles.text_black]}>%</Text>
-                    </View>
-                    <View style={[styles.cal_btn, styles.cal_btn_operation]}>
-                        <Text style={styles.cal_btn_text}>÷</Text>
-                    </View>
+{/* ........................... */}
+                    <View style={styles.main_btn_view_arrange}>
+                        <TouchableOpacity style={styles.cal_btn} onPress={() => { DeleteLog('AC') }}>
+                            <Text style={[styles.cal_btn_text, styles.text_black]}  >AC</Text>
+                        </TouchableOpacity>
 
-                    <View style={[styles.cal_btn, styles.cal_btn_number]}>
-                        <Text style={styles.cal_btn_text}>7</Text>
-                    </View>
-                    <View style={[styles.cal_btn, styles.cal_btn_number]}>
-                        <Text style={styles.cal_btn_text}>8</Text>
-                    </View>
-                    <View style={[styles.cal_btn, styles.cal_btn_number]}>
-                        <Text style={styles.cal_btn_text}>9</Text>
-                    </View>
-                    <View style={[styles.cal_btn, styles.cal_btn_operation]}>
-                        <Text style={styles.cal_btn_text}>×</Text>
-                    </View>
+                        <TouchableOpacity style={styles.cal_btn} onPress={() => { DeleteLog('X') }}>
+                            <View style={[styles.cal_btn_text, styles.text_black]}>
 
-                    <View style={[styles.cal_btn, styles.cal_btn_number]}>
-                        <Text style={styles.cal_btn_text}>4</Text>
-                    </View>
-                    <View style={[styles.cal_btn, styles.cal_btn_number]}>
-                        <Text style={styles.cal_btn_text}>5</Text>
-                    </View>
-                    <View style={[styles.cal_btn, styles.cal_btn_number]}>
-                        <Text style={styles.cal_btn_text}>6</Text>
-                    </View>
-                    <View style={[styles.cal_btn, styles.cal_btn_operation]}>
-                        <Text style={styles.cal_btn_text}>-</Text>
-                    </View>
+                                <Svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width={42}
+                                    height={42}
+                                    viewBox="1 -1 24 24"
+                                    fill="none"
+                                    stroke="#2a2a2a"
+                                    strokeWidth="1.4"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                >
+                                    <Path d="M20 5H9l-7 7 7 7h11a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2Z" />
+                                    <Line x1="17" x2="11" y1="9" y2="15" /><Line x1="11" x2="17" y1="9" y2="15" />
+                                </Svg>
+                            </View>
+                        </TouchableOpacity>
 
-                    <View style={[styles.cal_btn, styles.cal_btn_number]}>
-                        <Text style={styles.cal_btn_text}>1</Text>
-                    </View>
-                    <View style={[styles.cal_btn, styles.cal_btn_number]}>
-                        <Text style={styles.cal_btn_text}>2</Text>
-                    </View>
-                    <View style={[styles.cal_btn, styles.cal_btn_number]}>
-                        <Text style={styles.cal_btn_text}>3</Text>
-                    </View>
-                    <View style={[styles.cal_btn, styles.cal_btn_operation]}>
-                        <Text style={styles.cal_btn_text}>+</Text>
-                    </View>
+                        <TouchableOpacity style={styles.cal_btn} onPress={() => { HandelOnPress('%') }}>
+                            <View style={[styles.cal_btn_text, styles.text_black]}>
+                            <Svg
+                                    xmlns="http://www.w3.org/2000/svg" 
+                                    width="33" 
+                                    height="33" 
+                                    viewBox="0 -1 24 24"
+                                    fill="none" 
+                                    stroke="#2a2a2a" 
+                                    strokeWidth="1.8" 
+                                    stroke-linecap="round" 
+                                    stroke-linejoin="round"
+                                >
+                                    <Line x1="19" x2="5" y1="5" y2="19"/>
+                                    <Circle cx="7" cy="7" r="2.6"/>
+                                    <Circle cx="17" cy="17" r="2.5"/>
+                                </Svg>
+                            </View>
+                        </TouchableOpacity>
 
-                    <View style={[styles.cal_btn, styles.cal_btn_number,styles.take_two_width]}>
-                        <Text style={styles.cal_btn_text}>0</Text>
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_operation]} onPress={() => { HandelOnPress('÷') }}>
+                            <Text style={styles.cal_btn_text} >÷</Text>
+                        </TouchableOpacity>
                     </View>
-                    <View style={[styles.cal_btn, styles.cal_btn_number]}>
-                        <Text style={styles.cal_btn_text}>.</Text>
+                    
+{/* ........................................... */}
+                    <View style={styles.main_btn_view_arrange}>
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_number]} onPress={() => { HandelOnPress('7') }}>
+                            <Text style={styles.cal_btn_text} >7</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_number]} onPress={() => { HandelOnPress('8') }}>
+                            <Text style={styles.cal_btn_text}>8</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_number]} onPress={() => { HandelOnPress('9') }}>
+                            <Text style={styles.cal_btn_text}>9</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_operation]} onPress={() => { HandelOnPress('x') }}>
+                            <Text style={styles.cal_btn_text} >×</Text>
+                        </TouchableOpacity>
                     </View>
-                    <View style={[styles.cal_btn, styles.cal_btn_operation]}>
-                        <Text style={styles.cal_btn_text}>=</Text>
+                    
+{/* .................................................. */}
+                    <View style={styles.main_btn_view_arrange}>
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_number]} onPress={() => { HandelOnPress('4') }}>
+                            <Text style={styles.cal_btn_text} >4</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_number]} onPress={() => { HandelOnPress('5') }}>
+                            <Text style={styles.cal_btn_text}>5</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_number]} onPress={() => { HandelOnPress('6') }}>
+                            <Text style={styles.cal_btn_text} >6</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_operation]} onPress={() => { HandelOnPress('-') }}>
+                            <View style={styles.cal_btn_text}>
+                                <Svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="2 1 20 20"
+                                    fill="none"
+                                    stroke="white"
+                                    strokeWidth={2.5}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <Path d="M5 12h14" />
+                                </Svg>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                    
+{/* ........................................................ */}
+                    <View style={styles.main_btn_view_arrange}>
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_number]} onPress={() => { HandelOnPress('1') }}>
+                            <Text style={styles.cal_btn_text} >1</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_number]} onPress={() => { HandelOnPress('2') }}>
+                            <Text style={styles.cal_btn_text}>2</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_number]} onPress={() => { HandelOnPress('3') }}>
+                            <Text style={styles.cal_btn_text}>3</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_operation]} onPress={() => { HandelOnPress('+') }}>
+                            <Text style={styles.cal_btn_text}>+</Text>
+                        </TouchableOpacity>
+                    </View>
+                    
+{/* ................................................................ */}
+                    <View style={styles.main_btn_view_arrange}>
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_number, styles.take_two_width]} onPress={() => { HandelOnPress('0') }}>
+                            <Text style={styles.cal_btn_text}>0</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_number]} onPress={() => { HandelOnPress('.') }}>
+                            <Text style={styles.cal_btn_text}>.</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={[styles.cal_btn, styles.cal_btn_operation]} >
+                            <Text style={styles.cal_btn_text}>=</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
             </View>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    calculator_main: {
+        // backgroundColor:'red',
+        width: '88%',
+        height: '95%',
+        display: 'flex',
+        justifyContent:'space-between',
+        alignSelf:'center',
+        
+    },
+    output_view: {
+        // backgroundColor:'green',
+        width: '100%',
+        height: '47%'
+    },
+    input_view: {
+        // backgroundColor:'pink',
+        width: '100%',
+        height: '52%',
+
+    },
+    main_btn_view: {
+        // backgroundColor:'purple',
+        // backgroundColor:'blue',
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'space-between',
+        // flexDirection:'column'
+        rowGap:1,
+        // rowGap: 20,
+    },
+    main_btn_view_arrange:{
+        // backgroundColor:'pink',
+        width:'100%',
+        height:'18%',
+        display:'flex',
+        flexDirection:'row',
+        justifyContent: 'space-between',
+    },
+    cal_btn: {
+        width: '23%',
+        height: '100%',
+        backgroundColor: '#a5a5a5',
+        borderRadius: 10,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    take_two_width: {
+        width: '48%',
+        height: '100%',
+    },
+    cal_btn_text: {
+        fontSize: 30,
+        // fontWeight: '500',
+        color: 'white'
+    },
+    cal_btn_operation: {
+        backgroundColor: '#ff9f0a',
+        color: 'white',
+    },
+    cal_btn_number: {
+        backgroundColor: '#333333',
+        color: 'white',
+    },
+    text_black: {
+        color: '#2a2a2a',
+    },
+    final_output_container: {
+        // backgroundColor:'blue',
+        height: '85%',
+        width:'100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+    },
+    input_record: {
+        // backgroundColor:'gray',
+        width: '100%',
+        height: '15%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        paddingRight: 10,
+        overflow:'scroll',
+    },
+    final_output: {
+        flexGrow: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        paddingRight: 10,
+
+
+    },
+    final_output_text: {
+        // backgroundColor:'black',
+        fontSize: 45,
+        color: 'white',
+        textAlign: 'left'
+    },
+    input_record_text: {
+        fontSize: 30,
+        color: '#eeeeee',
+    }
+})
+
+
 export default Calculator;
